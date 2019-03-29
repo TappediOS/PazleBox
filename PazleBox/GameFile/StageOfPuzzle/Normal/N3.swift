6810e504b3968fbf7ae56c5fce75f91ed9de1b5b
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-03-29 14:36:34 +0900

    set

--- PazleBox/GameFile/StageOfPuzzle/Normal/N3.swift
+++ PazleBox/GameFile/StageOfPuzzle/Normal/N3.swift
@@ -14,21 +14,34 @@ class NStage3 {
    var Puzzle1: puzzle?
    var Puzzle2: puzzle?
    var Puzzle3: puzzle?
+   var Puzzle4: puzzle?
+   var Puzzle5: puzzle?
+   
+   var Hint1: HintPuzzle?
+   var Hint2: HintPuzzle?
    
    var PuzzleBox = Array<Any>()
+   var HintPuzzleBox = Array<Any>()
    
    init(ViewSizeX: CGFloat, ViewSizeY: CGFloat) {
       
       InitPuzzle(SizeX: ViewSizeX, SizeY: ViewSizeY)
       AddPuzzle()
+      
+      InitHint(SizeX: ViewSizeX, SizeY: ViewSizeY)
+      AddHintPuzzle()
    }
    
    private func InitPuzzle(SizeX: CGFloat, SizeY: CGFloat){
-      Puzzle1 = puzzle(PX: 3, PY: 3, CustNum: 1, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p9", PuzzleColor: "Red", RespawnX: 0, RespawnY: 2)
+      Puzzle1 = puzzle(PX: 3, PY: 3, CustNum: 0, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p1", PuzzleColor: "Red", RespawnX: 0, RespawnY: 4)
       
-      Puzzle2 = puzzle(PX: 2, PY: 1, CustNum: 2, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "21p1", PuzzleColor: "Blue", RespawnX: 0, RespawnY: 1)
+      Puzzle2 = puzzle(PX: 3, PY: 3, CustNum: 1, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p1", PuzzleColor: "Red", RespawnX: 4, RespawnY: 2)
       
-      //Puzzle3 = puzzle(PX: 3, PY: 2, CustNum: 3, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "32p9", PuzzleColor: "Red", RespawnX: 0, RespawnY: 2)
+      Puzzle3 = puzzle(PX: 3, PY: 2, CustNum: 2, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "32p12", PuzzleColor: "Blue", RespawnX: 0, RespawnY: 1)
+      
+      Puzzle4 = puzzle(PX: 3, PY: 2, CustNum: 3, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "32p7", PuzzleColor: "Blue", RespawnX: 3, RespawnY: 3)
+      
+      Puzzle5 = puzzle(PX: 3, PY: 3, CustNum: 4, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p17", PuzzleColor: "Green", RespawnX: 2, RespawnY: 2)
    }
    
    
@@ -36,11 +49,29 @@ class NStage3 {
    private func AddPuzzle() {
       PuzzleBox.append(Puzzle1!)
       PuzzleBox.append(Puzzle2!)
-      //PuzzleBox.append(Puzzle3!)
+      PuzzleBox.append(Puzzle3!)
+      PuzzleBox.append(Puzzle4!)
+      PuzzleBox.append(Puzzle5!)
       
    }
    
+   private func AddHintPuzzle() {
+      HintPuzzleBox.append(Hint1!)
+      HintPuzzleBox.append(Hint2!)
+   }
+   
+   private func InitHint(SizeX: CGFloat, SizeY: CGFloat) {
+      Hint1 = HintPuzzle(PX: 3, PY: 3, CustNum: 0, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p1", PuzzleColor: "Red", AnsX: 0, AnsY: 4)
+      
+      Hint2 = HintPuzzle(PX: 3, PY: 3, CustNum: 1, ViewX: Int(SizeX), ViewY: Int(SizeY), PuzzleStyle: "33p1", PuzzleColor: "Red", AnsX: 4, AnsY: 2)
+   }
+   
+   
    public func GetPuzzleBox() -> Array<Any> {
       return self.PuzzleBox
+   }
+   
+   public func GetHintPuzzleBox() -> Array<Any> {
+      return self.HintPuzzleBox
    }
 }
