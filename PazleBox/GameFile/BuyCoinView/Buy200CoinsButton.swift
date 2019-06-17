1fb14b3da2a67c390e4689f7287dbd0c135172eb
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-06-17 17:33:14 +0900

    add coin buy button

---
+++ PazleBox/GameFile/BuyCoinView/Buy200CoinsButton.swift
@@ -0,0 +1,24 @@
+//
+//  Buy200CoinsButton.swift
+//  PazleBox
+//
+//  Created by jun on 2019/06/17.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FlatUIKit
+import Firebase
+import ChameleonFramework
+
+class Buy200Coins: FUIButton {
+   
+   override init(frame: CGRect) {
+      super.init(frame: frame)
+   }
+   
+   required init?(coder aDecoder: NSCoder) {
+      fatalError()
+   }
+}
