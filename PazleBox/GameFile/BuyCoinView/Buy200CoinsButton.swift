acbdc97a493866946fa5c34013e5340ab7868d9b
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-06-17 17:39:34 +0900

    set code in coin button

--- PazleBox/GameFile/BuyCoinView/Buy200CoinsButton.swift
+++ PazleBox/GameFile/BuyCoinView/Buy200CoinsButton.swift
@@ -12,11 +12,27 @@ import FlatUIKit
 import Firebase
 import ChameleonFramework
 
-class Buy200Coins: FUIButton {
+class Buy200CoinsButton: FUIButton {
    
    override init(frame: CGRect) {
       super.init(frame: frame)
+      self.titleLabel?.adjustsFontSizeToFitWidth = true
+      self.titleLabel?.adjustsFontForContentSizeCategory = true
+      self.buttonColor = UIColor.turquoise()
+      self.shadowColor = UIColor.greenSea()
+      self.shadowHeight = 3.0
+      self.cornerRadius = 6.0
+      self.titleLabel?.font = UIFont.boldFlatFont (ofSize: 16)
+      self.setTitleColor(UIColor.clouds(), for: UIControl.State.normal)
+      self.setTitleColor(UIColor.clouds(), for: UIControl.State.highlighted)
+      self.setTitle(NSLocalizedString("200 coins", comment: ""), for: .normal)
+      self.addTarget(self, action: #selector(self.TapBuy200CoinsButton), for: .touchUpInside)
    }
+   
+   @objc func TapBuy200CoinsButton(sender: FUIButton) {
+      
+   }
+
    
    required init?(coder aDecoder: NSCoder) {
       fatalError()
