759251df4fbdd46fcb1b6820db059837878b5aa1
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-06-17 18:26:12 +0900

    コイン買うView形になってきた

--- PazleBox/GameFile/BuyCoinView/BuyCoinView.swift
+++ PazleBox/GameFile/BuyCoinView/BuyCoinView.swift
@@ -7,22 +7,28 @@
 //
 
 import UIKit
+import FlatUIKit
+import ChameleonFramework
 
 class BuyCoinView: UIView {
    
    var Buy200CoinButton: Buy200CoinsButton?
    var Buy650CoinButton: Buy650CoinsButton?
    var Buy1400CoinButton: Buy1400CoinsButton?
+   var BackToHomeButton: FUIButton?
    
    var ViewH: CGFloat = 0
    var ViewW: CGFloat = 0
    var FViewW: CGFloat = 0
    var FViewH: CGFloat = 0
+   var BackButtonSize: CGFloat = 0
+   var BackButtonInternal: CGFloat = 0
    
    override init(frame: CGRect) {
       super.init(frame: frame)
       InitBackGroundColor()
       InitViewSize()
+      InitBackToHomeButton()
       InitBuy200CoinButton()
       InitBuy650CoinButton()
       InitBuy1400CoinButton()
@@ -37,6 +43,8 @@ class BuyCoinView: UIView {
       ViewW = self.frame.width
       FViewW = ViewW / 25
       FViewH = ViewH / 32
+      BackButtonSize = ViewW / 5
+      BackButtonInternal = BackButtonSize / 5
    }
    
    private func InitBuy200CoinButton(){
@@ -55,6 +63,29 @@ class BuyCoinView: UIView {
       let Frame = CGRect(x: FViewW * 6, y: FViewH * 19, width: FViewW * 12, height: FViewH * 3)
       Buy1400CoinButton = Buy1400CoinsButton(frame: Frame)
       self.addSubview(Buy1400CoinButton!)
+   }
+   
+   private func InitBackToHomeButton() {
+      let FirstX = BackButtonInternal
+      let FirstY = BackButtonInternal
+      let Frame = CGRect(x: FirstX, y: FirstY, width: BackButtonSize, height: BackButtonSize / 2)
+      BackToHomeButton = FUIButton(frame: Frame)
+      BackToHomeButton?.setTitle("←", for: UIControl.State.normal)
+      BackToHomeButton?.buttonColor = UIColor.greenSea()
+      BackToHomeButton?.shadowColor = UIColor.greenSea()
+      BackToHomeButton?.shadowHeight = 3.0
+      BackToHomeButton?.cornerRadius = 6.0
+      BackToHomeButton?.titleLabel?.font = UIFont.boldFlatFont (ofSize: 16)
+      BackToHomeButton?.setTitleColor(UIColor.clouds(), for: UIControl.State.normal)
+      BackToHomeButton?.setTitleColor(UIColor.clouds(), for: UIControl.State.highlighted)
+      BackToHomeButton?.addTarget(self, action: #selector(self.TapBackToHomeButton(_:)), for: UIControl.Event.touchUpInside)
+      self.addSubview(BackToHomeButton!)
+   }
+   
+   @objc func TapBackToHomeButton (_ sender: FUIButton) {
+      self.fadeOut(type: .Normal){ [weak self] in
+         self?.removeFromSuperview()
+      }
    }
    
    required init?(coder aDecoder: NSCoder) {
