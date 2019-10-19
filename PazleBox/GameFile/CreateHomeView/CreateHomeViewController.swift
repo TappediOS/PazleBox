12cf454bcf13dbee36e7a5f29528a6e4f72a854d
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-10-19 12:37:05 +0900

    viewの内容を整えた

--- PazleBox/GameFile/CreateHomeView/CreateHomeViewController.swift
+++ PazleBox/GameFile/CreateHomeView/CreateHomeViewController.swift
@@ -14,10 +14,66 @@ import FlatUIKit
 class CreateHomeViewController: UIViewController {
    
    
+   @IBOutlet weak var SellectStageButton: FUIButton!
+   @IBOutlet weak var CreateStageButton: FUIButton!
+   @IBOutlet weak var BackHomeButton: FUIButton!
+   
+   var ViewW: CGFloat = 0
+   var ViewH: CGFloat = 0
+   var FViewW: CGFloat = 0
+   var FViewH: CGFloat = 0
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
-      view.backgroundColor = .white
+      InitViewSize()
+      InitEachButton()
+   }
+   
+   private func InitViewSize() {
+      ViewW = self.view.frame.width
+      ViewH = self.view.frame.height
+      FViewW = ViewW / 25
+      FViewH = ViewH / 32
+   }
+   
+   private func InitEachButton() {
+      InitButton(SellectStageButton)
+      InitButton(CreateStageButton)
+      InitButton(BackHomeButton)
+      SetUpButtonColor()
+      SetUpButtonPosition()
+   }
+   
+   private func InitButton(_ sender: FUIButton) {
+      sender.titleLabel?.adjustsFontSizeToFitWidth = true
+      sender.titleLabel?.adjustsFontForContentSizeCategory = true
+      sender.buttonColor = UIColor.turquoise()
+      sender.shadowColor = UIColor.greenSea()
+      sender.shadowHeight = 3.0
+      sender.cornerRadius = 6.0
+      sender.titleLabel?.font = UIFont.boldFlatFont (ofSize: 16)
+      sender.setTitleColor(UIColor.clouds(), for: UIControl.State.normal)
+      sender.setTitleColor(UIColor.clouds(), for: UIControl.State.highlighted)
+   }
+   
+   private func SetUpButtonColor() {
+      SellectStageButton.buttonColor = UIColor.flatPlum()
+      SellectStageButton.shadowColor = UIColor.flatPlumColorDark()
+      CreateStageButton.buttonColor = UIColor.flatTeal()
+      CreateStageButton.shadowColor = UIColor.flatTealColorDark()
+      BackHomeButton.buttonColor = UIColor.flatCoffee()
+      BackHomeButton.shadowColor = UIColor.flatCoffeeColorDark()
+   }
+   
+   private func SetUpButtonPosition() {
+      SellectStageButton.frame = CGRect(x: FViewW * 6, y: FViewH * 11, width: FViewW * 12, height: FViewH * 3)
+      CreateStageButton.frame = CGRect(x: FViewW * 6, y: FViewH * 15, width: FViewW * 12, height: FViewH * 3)
+      BackHomeButton.frame = CGRect(x: FViewW * 6, y: FViewH * 19, width: FViewW * 12, height: FViewH * 3)
+   }
+   
+   
+   @IBAction func TapBackButton(_ sender: Any) {
+      dismiss(animated: true, completion: nil)
    }
 }
