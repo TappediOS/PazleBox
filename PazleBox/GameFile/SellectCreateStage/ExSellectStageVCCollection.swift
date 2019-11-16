f4bdf690d55b051102800613925568e56caa196e
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-11-16 16:05:29 +0900

    画像をいい感じに

--- PazleBox/GameFile/SellectCreateStage/ExSellectStageVCCollection.swift
+++ PazleBox/GameFile/SellectCreateStage/ExSellectStageVCCollection.swift
@@ -18,8 +18,20 @@ extension SellectCreateStageViewController: UICollectionViewDelegate {
       }
       CanSellectStage = false
       print("Cellタップされた Cell: \(indexPath.item)")
-      LoadStageInfomation(CellNum: indexPath.item)
-      PresentGameViewController()
+      
+      var Image = UIImage()
+      let StageData = SavedStageDataBase.GetImageDataFromDataNumberASNSData(DataNum: indexPath.item)
+      if let data = StageData {
+         Image = UIImage(data: data as Data)!
+      }
+      
+      let flame = CGRect(x: view.frame.width / 10, y: view.frame.height / 4, width: view.frame.width / 10 * 8, height: view.frame.height / 2)
+      
+      let SellectedView = SellectView(frame: flame, Image: Image)
+      self.view.addSubview(SellectedView)
+      
+      //LoadStageInfomation(CellNum: indexPath.item)
+      //PresentGameViewController()
       Play3DtouchMedium()
       Analytics.logEvent("PlayCreateStageCount", parameters: nil)
     }
--- PazleBox/GameFile/SellectCreateStage/SellectView.swift
+++ PazleBox/GameFile/SellectCreateStage/SellectView.swift
@@ -6,62 +6,99 @@
 //  Copyright © 2019 jun. All rights reserved.
 //
 
-import Foundation
 import UIKit
-import FlatUIKit
 import ChameleonFramework
-import TapticEngine
+import FlatUIKit
 import Firebase
+import TapticEngine
 
+@IBDesignable
 class SellectView: UIView {
    
-   @IBOutlet weak var SellectImageView: UIImageView!
-   @IBOutlet weak var PlayButton: FUIButton!
-   @IBOutlet weak var DeleteButton: FUIButton!
    
+   @IBOutlet weak var SellectImageView: UIImageView!
+   @IBOutlet weak var PlayButton: UIButton!
+   @IBOutlet weak var DeleteButton: UIButton!
+   
+
    init(frame: CGRect, Image: UIImage) {
       super.init(frame: frame)
-   
+        
+      LoadNib()
       InitView()
       InitPlayButton()
       InitDeleteButton()
       InitImageView(Image: Image)
    }
-   
+     
+     
    private func InitView() {
-      self.layer.cornerRadius = 10
-      
+      self.layer.cornerRadius = 20
+      self.layer.masksToBounds = true
    }
-   
+     
    private func InitPlayButton() {
       PlayButton.titleLabel?.adjustsFontSizeToFitWidth = true
       PlayButton.titleLabel?.adjustsFontForContentSizeCategory = true
-      PlayButton.buttonColor = UIColor.flatLime()
-      PlayButton.shadowColor = UIColor.flatLimeColorDark()
-      PlayButton.shadowHeight = 3.0
-      PlayButton.cornerRadius = 6.0
       PlayButton.titleLabel?.font = UIFont.boldFlatFont (ofSize: 16)
       PlayButton.setTitleColor(UIColor.clouds(), for: UIControl.State.normal)
       PlayButton.setTitleColor(UIColor.clouds(), for: UIControl.State.highlighted)
+      PlayButton.layer.cornerRadius = 5
+      PlayButton.clipsToBounds = true
    }
-   
+     
    private func InitDeleteButton() {
       DeleteButton.titleLabel?.adjustsFontSizeToFitWidth = true
       DeleteButton.titleLabel?.adjustsFontForContentSizeCategory = true
-      DeleteButton.buttonColor = UIColor.flatRed()
-      DeleteButton.shadowColor = UIColor.flatRedColorDark()
-      DeleteButton.shadowHeight = 3.0
-      DeleteButton.cornerRadius = 6.0
       DeleteButton.titleLabel?.font = UIFont.boldFlatFont (ofSize: 16)
       DeleteButton.setTitleColor(UIColor.clouds(), for: UIControl.State.normal)
       DeleteButton.setTitleColor(UIColor.clouds(), for: UIControl.State.highlighted)
+      DeleteButton.layer.cornerRadius = 5
+      DeleteButton.clipsToBounds = true
    }
-   
+     
    private func InitImageView(Image: UIImage) {
       self.SellectImageView.image = Image
+      SellectImageView.layer.cornerRadius = SellectImageView.frame.size.width * 0.1
+      SellectImageView.clipsToBounds = true
+   }
+     
+     
+   private func LoadNib() {
+      let bundle = Bundle(for: type(of: self))
+      let nib = UINib(nibName: "SellectView", bundle: bundle)
+      let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+      addSubview(view)
+   }
+     
+   required init?(coder: NSCoder) {
+      super.init(coder: coder)
+      LoadNib()
+   }
+
+   
+   
+   // viewの枠線の色
+   @IBInspectable var borderColor: UIColor = UIColor.clear {
+       didSet {
+         self.layer.borderColor = borderColor.cgColor
+       }
+   }
+
+   // viewの枠線の太さ
+   @IBInspectable var borderWidth: CGFloat = 0 {
+       didSet {
+           self.layer.borderWidth = borderWidth
+       }
    }
    
-   required init?(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
+   // viewの角丸
+   @IBInspectable var cornerRadius: CGFloat = 0 {
+       didSet {
+           self.layer.cornerRadius = cornerRadius
+           self.layer.masksToBounds = true
+       }
    }
+   
+
 }
