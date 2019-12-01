33536156454ff91176b2c3167e58690da4cc1400
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-01 15:22:33 +0900

    ちゃんと動いた！！！ 完成？

--- PazleBox/GameFile/SellectInternetStage/ExSellectInternetVC.swift
+++ PazleBox/GameFile/SellectInternetStage/ExSellectInternetVC.swift
@@ -19,16 +19,17 @@ extension SellectInternetStageViewController {
          let PiceInfomation = PiceInfo()
          let PiceInfoName = "PiceInfo" + String(tmp)
          
-         let PiceArray = StageDic[PiceInfoName] as! Array<Any>
-         
-         PiceInfomation.PiceW = PiceArray[0] as! Int
-         PiceInfomation.PiceH = PiceArray[1] as! Int
-         PiceInfomation.ResX = PiceArray[2] as! Int
+         let PiceArrayFromDic = StageDic[PiceInfoName] as! Array<Any>
+
+         PiceInfomation.PiceW = PiceArrayFromDic[0] as! Int
+         PiceInfomation.PiceH = PiceArrayFromDic[1] as! Int
+         PiceInfomation.ResX = PiceArrayFromDic[2] as! Int
          //転置してるから11から引く必要がある
          //ResPownはのYは下から数えるから
-         PiceInfomation.ResY = 11 - PiceArray[3] as! Int
-         PiceInfomation.PiceName = PiceArray[4] as! String
-         PiceInfomation.PiceColor = PiceArray[0] as! String
+         PiceInfomation.ResY = 11 - (PiceArrayFromDic[3] as! Int)
+         PiceInfomation.PiceName = PiceArrayFromDic[4] as! String
+         PiceInfomation.PiceColor = PiceArrayFromDic[5] as! String
+         
          PiceArray.append(PiceInfomation)
       }
       return PiceArray
@@ -37,10 +38,11 @@ extension SellectInternetStageViewController {
    
    func GetPiceArrayFromDataBase(StageDic: [String: Any]) -> [[Contents]] {
       var StageArry: [[Contents]] = Array()
-      var StageXInfo: [Contents] = Array()
+      
       
       //Field1 から　Field12 まで回す
       for tmp in 1 ... 12 {
+         var StageXInfo: [Contents] = Array()
          let FieldName = "Field" + String(tmp)
          //FieldXの配列内容をInt型で取得
          //[0, 1, 1, 0, 1, 1, 0] みたいな
