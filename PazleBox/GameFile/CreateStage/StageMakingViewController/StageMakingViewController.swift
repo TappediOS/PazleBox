102df49a145bf1900d5a9a2b69d8879a8fde600f
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-19 13:41:09 +0900

    admobのデリゲートつけた

--- PazleBox/GameFile/CreateStage/StageMakingViewController/StageMakingViewController.swift
+++ PazleBox/GameFile/CreateStage/StageMakingViewController/StageMakingViewController.swift
@@ -244,4 +244,39 @@ class StageMakingViewController: UIViewController, GADBannerViewDelegate{
    private func Play3DtouchMedium() { TapticEngine.impact.feedback(.medium) }
    private func Play3DtouchHeavy()  { TapticEngine.impact.feedback(.heavy) }
    private func Play3DtouchError() { TapticEngine.notification.feedback(.error) }
+   
+   
+   //MARK:- ADMOB
+   /// Tells the delegate an ad request loaded an ad.
+   func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+      print("広告(banner)のロードが完了しました。")
+   }
+   
+   /// Tells the delegate an ad request failed.
+   func adView(_ bannerView: GADBannerView,
+               didFailToReceiveAdWithError error: GADRequestError) {
+      print("広告(banner)のロードに失敗しました。: \(error.localizedDescription)")
+   }
+   
+   /// Tells the delegate that a full-screen view will be presented in response
+   /// to the user clicking on an ad.
+   func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+      print("adViewWillPresentScreen")
+   }
+   
+   /// Tells the delegate that the full-screen view will be dismissed.
+   func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+      print("adViewWillDismissScreen")
+   }
+   
+   /// Tells the delegate that the full-screen view has been dismissed.
+   func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+      print("adViewDidDismissScreen")
+   }
+   
+   /// Tells the delegate that a user click will open another app (such as
+   /// the App Store), backgrounding the current app.
+   func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+      print("adViewWillLeaveApplication")
+   }
 }
