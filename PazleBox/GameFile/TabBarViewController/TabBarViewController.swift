233600e8e9392ae5d6676850c56331ce61531b30
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-30 14:13:24 +0900

    tabbarClassを追加した。

---
+++ PazleBox/GameFile/TabBarViewController/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2019/12/30.
+//  Copyright © 2019 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+   }
+}
