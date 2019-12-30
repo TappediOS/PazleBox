190804c6f675173e3885d840b320aa2052322bed
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-30 15:38:39 +0900

    tabbar にアイコン追加したよ。 sfsymbolってios13行こうやねんな。

--- PazleBox/GameFile/TabBarViewController/TabBarViewController.swift
+++ PazleBox/GameFile/TabBarViewController/TabBarViewController.swift
@@ -10,35 +10,99 @@ import UIKit
 import ESTabBarController_swift
 
 class PuzzleTabBarController: ESTabBarController {
+   
+   var image = UIImage()
+   var selectedImage = UIImage()
+   
    override func viewDidLoad() {
       super.viewDidLoad()
    }
     
    required init?(coder aDecoder: NSCoder) {
       super.init(coder: aDecoder)
+      
+      
+      image = getPlayImage()
+      selectedImage = getPlayFillImage()
         
       let Storybord = UIStoryboard(name: "CleateStageSB", bundle: nil)
       let PlayVC = Storybord.instantiateViewController(withIdentifier: "SellectCreateStageViewController")
       PlayVC.modalPresentationStyle = .fullScreen
       PlayVC.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "あそぶ",
-                                           image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"), tag: 1)
+                                           image: image, selectedImage: selectedImage, tag: 1)
       PlayVC.title = "あそぶ"
       
+      
+      image = getCreateImage()
+      selectedImage = getCreateFillImage()
       
       let CreateVC = Storybord.instantiateViewController(withIdentifier: "StageMakingVC")
       CreateVC.modalPresentationStyle = .fullScreen
       CreateVC.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "作る",
-                                               image: UIImage(named: "find"), selectedImage: UIImage(named: "find_1"), tag: 2)
+                                              image: image, selectedImage: selectedImage, tag: 2)
       CreateVC.title = "作る"
       
-        
-      let SettingVC = UIStoryboard(name: "Third", bundle: nil).instantiateInitialViewController()
-      SettingVC?.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "設定",
-                                                image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"), tag: 3)
-      SettingVC?.title = "設定"
+      
+      image = getSettingImage()
+      selectedImage = getSettingFillImage()
+      //TODO: ここのVCを作ってせっていしよう
+      //TODO: staticTableViewがいいかなー
+      let SettingVC = Storybord.instantiateViewController(withIdentifier: "StageMakingVC")
+      SettingVC.modalPresentationStyle = .fullScreen
+      SettingVC.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "設定",
+                                                image: image, selectedImage: selectedImage, tag: 3)
+      SettingVC.title = "設定"
         
       
         
-      self.viewControllers = [PlayVC, CreateVC, CreateVC]
+      self.viewControllers = [PlayVC, CreateVC, SettingVC]
+   }
+   
+   private func getPlayImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "play.circle")!
+      }else{
+         return UIImage(named: "play.png")!
+      }
+   }
+   
+   private func getPlayFillImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "play.circle.fill")!
+      }else{
+         return UIImage(named: "play_fill.png")!
+      }
+   }
+   
+   private func getCreateImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "hammer")!
+      }else{
+         return UIImage(named: "hammer.png")!
+      }
+   }
+   
+   private func getCreateFillImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "hammer.fill")!
+      }else{
+         return UIImage(named: "hammer_fill.png")!
+      }
+   }
+   
+   private func getSettingImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "person")!
+      }else{
+         return UIImage(named: "person.png")!
+      }
+   }
+   
+   private func getSettingFillImage() -> UIImage {
+      if #available(iOS 13, *) {
+         return UIImage(systemName: "person.fill")!
+      }else{
+         return UIImage(named: "person_fill.png")!
+      }
    }
 }
