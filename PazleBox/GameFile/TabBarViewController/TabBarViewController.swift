93af715e7cac02d1afdc5be72ad9f4c495dd3067
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2019-12-30 14:57:12 +0900

    tabbaerを追加したよ。

--- PazleBox/GameFile/TabBarViewController/TabBarViewController.swift
+++ PazleBox/GameFile/TabBarViewController/TabBarViewController.swift
@@ -6,16 +6,39 @@
 //  Copyright © 2019 jun. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import ESTabBarController_swift
 
-class TabBarViewController: UIViewController {
-   
-   
-   
-   
+class PuzzleTabBarController: ESTabBarController {
    override func viewDidLoad() {
       super.viewDidLoad()
+   }
+    
+   required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+        
+      let Storybord = UIStoryboard(name: "CleateStageSB", bundle: nil)
+      let PlayVC = Storybord.instantiateViewController(withIdentifier: "SellectCreateStageViewController")
+      PlayVC.modalPresentationStyle = .fullScreen
+      PlayVC.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "あそぶ",
+                                           image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"), tag: 1)
+      PlayVC.title = "あそぶ"
       
+      
+      let CreateVC = Storybord.instantiateViewController(withIdentifier: "StageMakingVC")
+      CreateVC.modalPresentationStyle = .fullScreen
+      CreateVC.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "作る",
+                                               image: UIImage(named: "find"), selectedImage: UIImage(named: "find_1"), tag: 2)
+      CreateVC.title = "作る"
+      
+        
+      let SettingVC = UIStoryboard(name: "Third", bundle: nil).instantiateInitialViewController()
+      SettingVC?.tabBarItem = ESTabBarItem.init(TabBarBasicContentView(), title: "設定",
+                                                image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"), tag: 3)
+      SettingVC?.title = "設定"
+        
+      
+        
+      self.viewControllers = [PlayVC, CreateVC, CreateVC]
    }
 }
