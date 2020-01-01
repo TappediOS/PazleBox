3f4140f8517dd1a48d0d5e438b7938fcafcea290
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-01 20:05:26 +0900

    ログインはなしで，ユーザ情報の切り替えにする。3

--- PazleBox/GameFile/SettingViewController/TapUserInfo/ExSettingUserInfo.swift
+++ PazleBox/GameFile/SettingViewController/TapUserInfo/ExSettingUserInfo.swift
@@ -13,21 +13,12 @@ import UIKit
 
 extension SettingTableViewController {
    func TapUserInfo() {
-      guard let user = Auth.auth().currentUser else {
-         print("ユーザがいない")
-         return
-      }
-      
-      if user.isAnonymous {
-         showFirebaseLoginVC()
-         return
-      }
-   
+      showFirebaseLoginVC()
    }
    
    func showFirebaseLoginVC() {
-      let Storybord = UIStoryboard(name: "FirebaseLoginSB", bundle: nil)
-      let LoginVC = Storybord.instantiateViewController(withIdentifier: "FirebaseLoginVC")
+      let Storybord = UIStoryboard(name: "UsersSetting", bundle: nil)
+      let LoginVC = Storybord.instantiateViewController(withIdentifier: "UsersSettingVC")
       LoginVC.modalPresentationStyle = .pageSheet
       present(LoginVC, animated: true, completion: {
          print("Login画面にプレゼント完了")
