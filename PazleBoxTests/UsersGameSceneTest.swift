2f62f4e574a7fbe65c01770d6751d1e66fd7c38c
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-11 14:35:35 +0900

    testCheckLeftUpのテスト記述

--- PazleBoxTests/UsersGameSceneTest.swift
+++ PazleBoxTests/UsersGameSceneTest.swift
@@ -33,8 +33,25 @@ class UsersGameSceneTest: XCTestCase {
    }
    
    
-   func testGetSeceneScalaMode() {
-      GameScene.CheckLeftUp()
+   func testCheckLeftUp() {
+      XCTContext.runActivity(named: "xが0以上大きいかつyが11以下の時はTrueである", block: { _ in
+         XCTAssertTrue(GameScene.CheckLeftUp(x: 0, y: 11))
+         XCTAssertTrue(GameScene.CheckLeftUp(x: 3, y: 3))
+         XCTAssertTrue(GameScene.CheckLeftUp(x: 10, y: 0))
+         XCTAssertTrue(GameScene.CheckLeftUp(x: 100, y: -1))
+      })
+      
+      XCTContext.runActivity(named: "xがマイナスの時はFalseである。", block: { _ in
+         XCTAssertFalse(GameScene.CheckLeftUp(x: -1, y: 11))
+         XCTAssertFalse(GameScene.CheckLeftUp(x: -4, y: 20))
+         XCTAssertFalse(GameScene.CheckLeftUp(x: -10, y: -12))
+      })
+      
+      XCTContext.runActivity(named: "yが11より大きい時はFalseである。", block: { _ in
+         XCTAssertFalse(GameScene.CheckLeftUp(x: -4, y: 13))
+         XCTAssertFalse(GameScene.CheckLeftUp(x: 0, y: 456))
+         XCTAssertFalse(GameScene.CheckLeftUp(x: 23, y: 56))
+      })
    }
 
 }
