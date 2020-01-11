d3b9ae34321b9bfc8ed1ba5a40860602965cf67a
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-11 15:12:34 +0900

    Puzzleの情報が正しいかどうかのテストの記述

--- PazleBoxTests/PuzzleTest.swift
+++ PazleBoxTests/PuzzleTest.swift
@@ -32,6 +32,25 @@ class PuzzleTest: XCTestCase {
 
    //Puzzleの情報が正しいかどうかのテスト。
    func testGetOfInfomation() {
+      let StartPointX = 3
+      let StartPointY =  5
+      let PuzzleWide = 4
+      let PuzzleHight = 3
+      let PuzzleStyle = "43p9"
       
+      let PuzzleArry = PArry()
+      let PuzzlePArry = PuzzleArry.GerPArry(PuzzleStyle: PuzzleStyle)
+      
+      let Puzzle = puzzle(PX: PuzzleWide, PY: PuzzleHight, CustNum: 2, ViewX: 50, ViewY: 50, PuzzleStyle: "43p9", PuzzleColor: "Red", RespawnX: StartPointX, RespawnY: StartPointY)
+      
+      let exp = Puzzle.GetOfInfomation()
+      
+      XCTContext.runActivity(named: "ゲットしたピースの情報が正しいこと", block: { _ in
+         XCTAssertEqual(exp["StartPointX"] as! Int, StartPointX)
+         XCTAssertEqual(exp["StartPointY"] as! Int, StartPointY)
+         XCTAssertEqual(exp["PuzzleWide"] as! Int, PuzzleWide)
+         XCTAssertEqual(exp["PuzzleHight"] as! Int, PuzzleHight)
+         XCTAssertEqual(exp["PArry"] as! Array, PuzzlePArry)
+      })
    }
 }
