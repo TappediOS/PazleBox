41b3573c166cf93a3d66b165fedf4bfb8a785379
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-12 15:34:47 +0900

    SellectInterNetStageVCTestファイルを追加

---
+++ PazleBoxUITests/SellectInterNetStageVCTest.swift
@@ -0,0 +1,24 @@
+//
+//  SellectInterNetStageVCTest.swift
+//  PazleBoxUITests
+//
+//  Created by jun on 2020/01/12.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+
+class SellectInterNetStageVCTest: XCTestCase {
+   let app = XCUIApplication()
+   
+   override func setUp() {
+     continueAfterFailure = false
+     let app = XCUIApplication()
+     setupSnapshot(app)
+     app.launch()
+   }
+
+   override func tearDown() {
+      super.tearDown()
+   }
+}
