739464b90ff09f6ce2a02647164ddca9afdb2fe0
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-12 12:34:44 +0900

    TabbarのUITestを追加

---
+++ PazleBoxUITests/TopTabBarTest.swift
@@ -0,0 +1,29 @@
+//
+//  TopTabBarTest.swift
+//  PazleBoxUITests
+//
+//  Created by jun on 2020/01/12.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+
+class TopTabBarUITest: XCTestCase {
+   
+   let app = XCUIApplication()
+   
+   override func setUp() {
+     continueAfterFailure = false
+     let app = XCUIApplication()
+     setupSnapshot(app)
+     app.launch()
+   }
+
+   override func tearDown() {
+       // Put teardown code here. This method is called after the invocation of each test method in the class.
+   }
+   
+   
+
+   
+}
