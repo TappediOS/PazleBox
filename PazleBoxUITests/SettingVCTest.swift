bec615b2f1dc34720381872b0b227f481fb3178d
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-12 13:06:56 +0900

    SettingVCTestファイルの追加

---
+++ PazleBoxUITests/SettingVCTest.swift
@@ -0,0 +1,27 @@
+//
+//  SettingVCTest.swift
+//  PazleBoxUITests
+//
+//  Created by jun on 2020/01/12.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+
+class SettingVCTest: XCTestCase {
+   let app = XCUIApplication()
+   
+   override func setUp() {
+     continueAfterFailure = false
+     let app = XCUIApplication()
+     setupSnapshot(app)
+     app.launch()
+   }
+
+   override func tearDown() {
+       // Put teardown code here. This method is called after the invocation of each test method in the class.
+   }
+   
+   
+   
+}
