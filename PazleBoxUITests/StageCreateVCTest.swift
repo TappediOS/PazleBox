90fff50571c04782b94c0ca550b054d6038fbe45
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-12 13:47:36 +0900

    StageCreateVCTestを追加

---
+++ PazleBoxUITests/StageCreateVCTest.swift
@@ -0,0 +1,24 @@
+//
+//  StageCreateVCTest.swift
+//  PazleBoxUITests
+//
+//  Created by jun on 2020/01/12.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import XCTest
+
+class StageCreateVCTest: XCTestCase {
+   let app = XCUIApplication()
+   
+   override func setUp() {
+     continueAfterFailure = false
+     let app = XCUIApplication()
+     setupSnapshot(app)
+     app.launch()
+   }
+
+   override func tearDown() {
+      super.tearDown()
+   }
+}
