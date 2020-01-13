d8ed1cc508115d61cc7588c5da31afb9f9148a56
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-13 17:02:26 +0900

    UserAgreementViewで必要な処理を書いた

--- PazleBox/GameFile/UserAgreement/UserAgreementView.swift
+++ PazleBox/GameFile/UserAgreement/UserAgreementView.swift
@@ -6,4 +6,85 @@
 //  Copyright © 2020 jun. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import ChameleonFramework
+import FlatUIKit
+import Firebase
+import FirebaseFirestore
+import TapticEngine
+
+@IBDesignable
+class UserAgreementView: UIView {
+
+   init(frame: CGRect, Image: UIImage, CellNum: Int, PlayCount: Int, ReviewAve: CGFloat, addDate: String, addUserUID: String) {
+      super.init(frame: frame)
+      
+      if #available(iOS 13.0, *) {
+         self.overrideUserInterfaceStyle = .light
+      }
+      
+      
+      LoadNib()
+      InitView()
+
+   }
+   
+   override func awakeFromNib() {
+      
+   }
+   
+
+   private func LoadNib() {
+      let bundle = Bundle(for: type(of: self))
+      let nib = UINib(nibName: "InterNetSellect", bundle: bundle)
+      let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+      view.frame = self.bounds
+      addSubview(view)
+   }
+   
+   private func InitView() {
+      self.layer.borderColor = UIColor.black.cgColor
+      self.layer.borderWidth = 0.45
+      self.layer.shadowOffset = CGSize(width: 14.5, height: 14.5)
+      self.layer.shadowColor = UIColor.black.cgColor
+      //1にすれば真っ黒，0にすれば透明に
+      self.layer.shadowOpacity = 0.65
+      self.layer.cornerRadius = 5.8
+   }
+   
+
+  
+
+
+   // viewの枠線の色
+   @IBInspectable var borderColor: UIColor = UIColor.clear {
+      didSet {
+      self.layer.borderColor = borderColor.cgColor
+      }
+   }
+
+      // viewの枠線の太さ
+   @IBInspectable var borderWidth: CGFloat = 0 {
+      didSet {
+         self.layer.borderWidth = borderWidth
+      }
+   }
+      
+      // viewの角丸
+   @IBInspectable var cornerRadius: CGFloat = 0 {
+      didSet {
+         self.layer.cornerRadius = cornerRadius
+         self.layer.masksToBounds = true
+      }
+   }
+      
+      
+   func Play3DtouchLight()  { TapticEngine.impact.feedback(.light) }
+   func Play3DtouchMedium() { TapticEngine.impact.feedback(.medium) }
+   func Play3DtouchHeavy()  { TapticEngine.impact.feedback(.heavy) }
+      
+   required init?(coder: NSCoder) {
+      super.init(coder: coder)
+      LoadNib()
+   }
+}
