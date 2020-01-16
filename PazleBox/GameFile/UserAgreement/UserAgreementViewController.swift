2db4671a60ce6c9254871e2bea4fc693bf665696
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-16 13:49:35 +0900

    UserAgreementViewControllerの追加をした

---
+++ PazleBox/GameFile/UserAgreement/UserAgreementViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UserAgreementViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/01/16.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserAgreementViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      
+      
+   }
+}
