2cdf599f30fa88a628f302a4b5aa9af9e3f0aef7
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-16 13:58:07 +0900

    agreementView 表示完了。

--- PazleBox/GameFile/UserAgreement/UserAgreementViewController.swift
+++ PazleBox/GameFile/UserAgreement/UserAgreementViewController.swift
@@ -13,16 +13,34 @@ class UserAgreementViewController: UIViewController {
    
    var BackGroundImageView: BackGroundImageViews?
    
+   var UserAgreementViewFrame = CGRect()
+   
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       
+      InitAgreementViewSizeSetting()
       InitBackgroundImageView()
-      
+      InitUserAgreementView()
+   }
+   
+   private func InitAgreementViewSizeSetting() {
+      let StartX = view.frame.width / 16
+      let StartY = view.frame.height / 4
+      let ViewSize = view.frame.width / 16 * 14
+      UserAgreementViewFrame = CGRect(x: StartX, y: StartY, width: ViewSize, height: ViewSize)
    }
    
    private func InitBackgroundImageView() {
       BackGroundImageView = BackGroundImageViews(frame: self.view.frame)
       self.view.addSubview(BackGroundImageView!)
       self.view.sendSubviewToBack(BackGroundImageView!)
+   }
+   
+   private func InitUserAgreementView() {
+      let AgreeView = UserAgreementView(frame: UserAgreementViewFrame)
+      AgreeView.center.y = view.center.y
+      AgreeView.center.x = view.center.x
+      self.view.addSubview(AgreeView)
    }
 }
