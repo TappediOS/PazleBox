425cd1f7120309ef266079a313aa3ed7aabd881e
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-17 17:06:41 +0900

    アニメーションを追加

--- PazleBox/GameFile/Tutorial/TutorialManager/tapImage.swift
+++ PazleBox/GameFile/Tutorial/TutorialManager/tapImage.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import ViewAnimator
 
 class TapImage: UIImageView {
    
@@ -21,5 +22,21 @@ class TapImage: UIImageView {
    
    required init?(coder: NSCoder) {
       fatalError("init(coder:) has not been implemented")
+   }
+   
+   //MARK:- 画像を消したり出したりする。
+   public func clearImageView() { self.alpha = 0 }
+   public func appearImageView() { self.alpha = 1 }
+   
+   public func changePosition(posiX: CGFloat, posiY: CGFloat) {
+      let frame = CGRect(x: posiX, y: posiY, width: self.frame.width, height: self.frame.height)
+      self.frame = frame
+   }
+   
+   public func startAnimation() {
+      let animation = AnimationType.zoom(scale: 2)
+      self.animate(animations: [animation], delay: 3.0, duration: 2.0, options: [.repeat, .curveEaseInOut], completion: {
+         print("アニメーション")
+      })
    }
 }
