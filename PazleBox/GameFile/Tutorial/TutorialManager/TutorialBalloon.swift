8f7359204f5b54b82c9365b8d5e7e4a8d6de8a3c
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-17 14:38:20 +0900

    吹き出しのViewの設定ファイルを作成

---
+++ PazleBox/GameFile/Tutorial/TutorialManager/TutorialBalloon.swift
@@ -0,0 +1,14 @@
+//
+//  TutorialBalloon.swift
+//  PazleBox
+//
+//  Created by jun on 2020/01/17.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TutorialBalloon: UIView {
+   
+}
