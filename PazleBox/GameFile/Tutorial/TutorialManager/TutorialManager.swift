a52d22766e3bb96aea1a4ab2f3923b9605ab5204
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-18 11:52:11 +0900

    チュートリアルナンバーの追加

--- PazleBox/GameFile/Tutorial/TutorialManager/TutorialManager.swift
+++ PazleBox/GameFile/Tutorial/TutorialManager/TutorialManager.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+enum TutorialNum {
+   case tutorial1
+   case tutorial2
+   case tutorial3
+   case tutorial4
+   case tutorial5
+   case tutorial6
+   case tutorial7
+   case tutorial8
+   case tutorial9
+   case tutorial10_1
+   case tutorial10_2
+   case tutorial11
+   case tutorial12
+   case tutorial13_1
+   case tutorial13_2
+   case tutorial14
+   case tutorial15
+}
+
 enum TutorialState {
    case none
    case wait      //まつ
@@ -17,9 +37,13 @@ enum TutorialState {
 
 class TutorialManager {
    var state = TutorialState.none
+   var tutorialNum = TutorialNum.tutorial1
    
    init() {
       state = .advance
+      tutorialNum = .tutorial1
    }
    
+   public func getState() -> TutorialState { return self.state }
+   public func getTutorialNum() -> TutorialNum { return self.tutorialNum }
 }
