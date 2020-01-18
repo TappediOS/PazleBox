a16d6a3db1e0b993a8a2f87e7e2b55377770f14f
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-18 11:43:38 +0900

    TutorialManagerクラスを追加

---
+++ PazleBox/GameFile/Tutorial/TutorialManager/TutorialManager.swift
@@ -0,0 +1,15 @@
+//
+//  TutorialManager.swift
+//  PazleBox
+//
+//  Created by jun on 2020/01/18.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+
+enum TutorialState {
+
+class TutorialManager {
+   
+}
