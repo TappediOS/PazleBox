d3e81054459bfdb37b14940b257e713f4723a969
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-01-18 11:47:27 +0900

    stateのenumを作った

--- PazleBox/GameFile/Tutorial/TutorialManager/TutorialManager.swift
+++ PazleBox/GameFile/Tutorial/TutorialManager/TutorialManager.swift
@@ -9,7 +9,17 @@
 import Foundation
 
 enum TutorialState {
+   case none
+   case wait      //まつ
+   case advance   //文字を進める状態
+   case operaton  //ユーザが操作する状態
+}
 
 class TutorialManager {
+   var state = TutorialState.none
+   
+   init() {
+      state = .advance
+   }
    
 }
