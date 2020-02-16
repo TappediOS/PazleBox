d75822904bd1ae43d5568f0b9568605f3aceaf46
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-16 16:40:07 +0900

    ShoppingPiceVCの追加

---
+++ PazleBox/GameFile/ShoppingPiceViewController/ShoppingPiceViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingPiceViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/16.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ShoppingPiceViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+   }
+}
