bca7e594d385bca2c3cae941a3e645c28d0a42c0
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-16 17:20:35 +0900

    背景画像を入れた

--- PazleBox/GameFile/ShoppingPiceViewController/ShoppingPiceViewController.swift
+++ PazleBox/GameFile/ShoppingPiceViewController/ShoppingPiceViewController.swift
@@ -10,9 +10,16 @@ import Foundation
 import UIKit
 
 class ShoppingPiceViewController: UIViewController {
+   var BackGroundImageView: BackGroundImageViews?
    
    override func viewDidLoad() {
       super.viewDidLoad()
-      
+      InitBackgroundImageView()
+   }
+   
+   private func InitBackgroundImageView() {
+      BackGroundImageView = BackGroundImageViews(frame: self.view.frame)
+      self.view.addSubview(BackGroundImageView!)
+      self.view.sendSubviewToBack(BackGroundImageView!)
    }
 }
