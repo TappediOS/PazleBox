56add7fc62dc7cd04178cf26809fefae0b33718a
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-20 17:08:54 +0900

    tableviewのcellのファイルを追加

---
+++ PazleBox/GameFile/SellectInternetStage/InterNetTableView/InterNexTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InterNexTableViewCell.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/20.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class InterNexTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
