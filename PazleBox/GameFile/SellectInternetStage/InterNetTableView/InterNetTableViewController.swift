780179f94b9a46633401076ece7a0c5fa1043498
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-21 14:17:22 +0900

    リロードできるようにした

--- PazleBox/GameFile/SellectInternetStage/InterNetTableView/InterNetTableViewController.swift
+++ PazleBox/GameFile/SellectInternetStage/InterNetTableView/InterNetTableViewController.swift
@@ -11,16 +11,25 @@ import UIKit
 
 class InterNetTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
    
-   
-   
    @IBOutlet weak var InterNetTableView: UITableView!
-   
+   var RefleshControl = UIRefreshControl()
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
       self.InterNetTableView.delegate = self
       self.InterNetTableView.dataSource = self
+      
+      SetUpRefleshControl()
+   }
+   
+   func SetUpRefleshControl() {
+      self.InterNetTableView.refreshControl = self.RefleshControl
+      self.RefleshControl.addTarget(self, action: #selector(self.ReloadDataFromFireStore(sender:)), for: .valueChanged)
+   }
+   
+   @objc func ReloadDataFromFireStore(sender: UIRefreshControl) {
+      RefleshControl.endRefreshing()
    }
 }
 
