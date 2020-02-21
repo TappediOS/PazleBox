92588f735d4e95ffb56c2e6975bba2d2ec372e49
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-21 14:20:22 +0900

    cellがラップされたときの表示VCのファイルを追加

---
+++ PazleBox/GameFile/SellectInternetStage/InterNetTableView/InterNetCellTappedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InterNetCellTappedViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/21.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InterNetCellTappedViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+   }
+}
