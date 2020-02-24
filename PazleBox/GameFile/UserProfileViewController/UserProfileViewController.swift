434a5c61afb463386033090c7e847582c4b85157
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-24 13:54:58 +0900

    UserProfileViewControllerを追加

---
+++ PazleBox/GameFile/UserProfileViewController/UserProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/24.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserProfileViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+}
