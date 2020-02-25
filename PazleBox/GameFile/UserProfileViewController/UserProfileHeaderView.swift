dce84b42d282c1e878d48efd099935346ab427ea
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-25 11:28:50 +0900

    ヘッダviewのカスタムクラスを生成した

---
+++ PazleBox/GameFile/UserProfileViewController/UserProfileHeaderView.swift
@@ -0,0 +1,28 @@
+//
+//  UserProfileHeaderView.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/25.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserProfileHeaderView: UIView {
+   override init(frame: CGRect){
+      super.init(frame: frame)
+      loadNib()
+   }
+   
+   required init(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)!
+      loadNib()
+   }
+   
+   func loadNib(){
+      let view = Bundle.main.loadNibNamed("UserProfileHeaderView", owner: self, options: nil)?.first as! UIView
+      view.frame = self.bounds
+      self.addSubview(view)
+   }
+}
