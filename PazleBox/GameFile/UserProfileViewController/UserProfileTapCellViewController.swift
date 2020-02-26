a424d0c9346c7203024931cc8cddfab62ac8f946
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-26 13:10:30 +0900

    UsersProfileVCとファイルを繋いだ

--- PazleBox/GameFile/UserProfileViewController/UserProfileTapCellViewController.swift
+++ PazleBox/GameFile/UserProfileViewController/UserProfileTapCellViewController.swift
@@ -11,8 +11,39 @@ import UIKit
 
 class UserProfileTapCellViewController: UIViewController {
    
+   
+   @IBOutlet weak var UsersProfileImageView: UIImageView!
+   @IBOutlet weak var UsersNameLabel: UILabel!
+   
+   var UsersProfileImage = UIImage()
+   var UsersName: String = ""
+   
+   @IBOutlet weak var UsersPostedStageImageView: UIImageView!
+   @IBOutlet weak var UsersPostedStageTitleLabel: UILabel!
+   
+   var UsersPostedStageImage = UIImage()
+   var UsersPostedStageTitle: String = ""
+   
+   @IBOutlet weak var UsersPostedStageReviewLabel: UILabel!
+   @IBOutlet weak var UsersPostedStagePalyCountLabel: UILabel!
+   
+   var UsersPostedStageReview: String = "" // 2.23 / 5
+   var UsersPostedStagePlayCount: String = "" // 245
+   
+   @IBOutlet weak var UsersPostedStagePlayButton: UIButton!
+   @IBOutlet weak var UsersPostedStageDeleteButton: UIButton!
+   
+   
    override func viewDidLoad() {
       super.viewDidLoad()
       
    }
+   
+   @IBAction func TapUsersPostedStagePlayButton(_ sender: Any) {
+   }
+   
+   
+   @IBAction func TapUsersPostedStagePlayButton(_ sender: Any) {
+   }
+   
 }
