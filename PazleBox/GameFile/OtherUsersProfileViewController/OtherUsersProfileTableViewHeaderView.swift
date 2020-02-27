e98fd07e45093685739e7cca18e80736aef784da
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-27 16:04:33 +0900

    OtherUsersProfileTableViewHeaderViewを追加

---
+++ PazleBox/GameFile/OtherUsersProfileViewController/OtherUsersProfileTableViewHeaderView.swift
@@ -0,0 +1,27 @@
+//
+//  OtherUsersProfileTableViewHeaderView.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/27.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import UIKit
+
+class OtherUsersProfileTableViewHeaderView: UIView {
+   override init(frame: CGRect){
+      super.init(frame: frame)
+      loadNib()
+   }
+      
+   required init(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)!
+      loadNib()
+   }
+      
+   func loadNib(){
+      let view = Bundle.main.loadNibNamed("OtherUesrsProfileTableViewHeaderView", owner: self, options: nil)?.first as! UIView
+      view.frame = self.bounds
+      self.addSubview(view)
+   }
+}
