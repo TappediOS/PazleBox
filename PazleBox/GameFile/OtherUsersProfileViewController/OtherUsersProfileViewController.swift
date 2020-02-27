ca9b593f364d793c78656f90b9022916c2e4ae7b
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-27 15:46:53 +0900

    OtherUesrsProfileTableViewを接続した

--- PazleBox/GameFile/OtherUsersProfileViewController/OtherUsersProfileViewController.swift
+++ PazleBox/GameFile/OtherUsersProfileViewController/OtherUsersProfileViewController.swift
@@ -9,10 +9,84 @@
 import Foundation
 import UIKit
 
-class OtherUsersProfileViewController: UIViewController {
+class OtherUsersProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
    
+   @IBOutlet weak var OtherUesrsProfileTableView: UITableView!
+   private let sectionHeaderHeight: CGFloat = 200
+      
+   var RefleshControl = UIRefreshControl()
+      
+      
    override func viewDidLoad() {
       super.viewDidLoad()
+         
+      SetUpRefleshControl()
+         
+      OtherUesrsProfileTableView.rowHeight = 160
+      OtherUesrsProfileTableView.delegate = self
+      OtherUesrsProfileTableView.dataSource = self
+   }
       
+   func SetUpRefleshControl() {
+      self.OtherUesrsProfileTableView.refreshControl = self.RefleshControl
+      self.RefleshControl.addTarget(self, action: #selector(self.ReloadDataFromFireStore(sender:)), for: .valueChanged)
+   }
+      
+   @objc func ReloadDataFromFireStore(sender: UIRefreshControl) {
+      RefleshControl.endRefreshing()
+   }
+}
+
+
+extension OtherUsersProfileViewController {
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return 25
+   }
+   
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = self.OtherUesrsProfileTableView.dequeueReusableCell(withIdentifier: "UserProfileTableCell", for: indexPath) as? UserProfileTableViewCell
+      
+
+      return cell!
+   }
+   
+   //ヘッダーの高さを設定
+   func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat{
+      return sectionHeaderHeight
+   }
+   
+   //ヘッダーに使うUIViewを返す
+   func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?{
+      //xibファイルから読み込んでヘッダを生成
+      let HeaderView = UserProfileHeaderView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: sectionHeaderHeight))
+      return HeaderView
+   }
+   
+   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      // セルの選択を解除する
+      tableView.deselectRow(at: indexPath, animated: true)
+
+      //self.navigationController?.pushViewController(UserProfileTapCellViewController, animated: true)
+   }
+   
+   
+   //スクロールした際にtableviewのヘッダを動かす
+   func scrollViewDidScroll(_ scrollView: UIScrollView) {
+       /// sectionHeaderが上部に残らないようにする
+       let offsetY = scrollView.contentOffset.y
+       let safeAreaInset: CGFloat = scrollView.safeAreaInsets.top
+
+       let top: CGFloat
+       if offsetY > sectionHeaderHeight{
+           /// 一番上のheaderの最下部が画面外へ出ている状態
+           top = -(safeAreaInset + sectionHeaderHeight)
+       } else if offsetY < -safeAreaInset {
+           /// 初期状態からメニューを下に引っ張っている状態
+           top = 0
+       } else {
+           /// safeArea内を一番上のheaderが移動している状態
+           top = -(safeAreaInset + offsetY)
+       }
+       scrollView.contentInset = UIEdgeInsets(top: top, left: 0, bottom: 0, right: 0)
    }
 }
