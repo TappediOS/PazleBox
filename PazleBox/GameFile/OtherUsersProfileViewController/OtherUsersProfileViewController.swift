232b793fbdde42ec04c615572f3e921870d50aaf
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-02-27 15:28:00 +0900

    OtherUsersProfileViewControllerの追加

---
+++ PazleBox/GameFile/OtherUsersProfileViewController/OtherUsersProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OtherUsersProfileViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/02/27.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OtherUsersProfileViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+   }
+}
