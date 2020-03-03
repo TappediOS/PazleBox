99f85713416a2dd2fde0ddcc72c77fd68e7227ef
TappediOS <eu.bkw7jdcpa2r@gmail.com>
2020-03-03 10:12:10 +0900

    EditProfileVCの追加

---
+++ PazleBox/GameFile/UserProfileViewController/EditProfileViewController/EditProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EditProfileViewController.swift
+//  PazleBox
+//
+//  Created by jun on 2020/03/03.
+//  Copyright © 2020 jun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditProfileViewController: UIViewController {
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      
+   }
+}
